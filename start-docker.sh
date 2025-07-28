6e4ee463c1a2e15fb207d52475666a2f4905b6a5
pehlivanyunuscan <pehlivanyunuscan@hotmail.com>
2025-07-28 11:26:00 +0300

    fix(docker): update start-docker.sh script

--- start-docker.sh
+++ start-docker.sh
@@ -8,4 +8,5 @@ sudo docker-compose down
 echo "Building Docker image and starting websocket-gochat service..."
 sudo docker-compose up --build -d
 sudo docker-compose logs -f
+
 echo "WebSocket GoChat is running"
