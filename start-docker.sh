3893dd3617a0923f5ebe1e12d11a3db0187ca847
pehlivanyunuscan <pehlivanyunuscan>
2025-07-28 09:01:29 +0300

    fix(docker): update start-docker.sh script

--- start-docker.sh
+++ start-docker.sh
@@ -4,13 +4,8 @@ set -e
 
 echo "Stopping and removing old containers..."
 sudo docker-compose down
-sudo docker-compose rm -f $(sudo docker-compose ps -aq)
-
-echo "Pruning unused volumes and networks..."
-sudo docker volume prune -f
-sudo docker network prune -f
 
 echo "Building Docker image and starting websocket-gochat service..."
 sudo docker-compose up --build -d
-
+sudo docker-compose logs -f
 echo "WebSocket GoChat is running"
